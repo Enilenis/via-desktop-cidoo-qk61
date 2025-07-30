e2aad61c104750ea9d1342a1869914b59e929090
Matthew <dimblaze@gmail.com>
2025-07-30 18:50:54 +0400

    Synced .json config with current CIDOO-QK61 firmware build

--- windows.bat
+++ windows.bat
@@ -3,12 +3,11 @@ xcopy via-desktop\* .\ /E /Y
 rmdir via-desktop /S /Q
 
 git clone https://github.com/the-via/app.git via-app
-xcopy via-desktop-cidoo-qk61\* .\ /E /Y
-rmdir via-desktop-cidoo-qk61 /S /Q
-
 cd via-app
 call npm install
 powershell -Command "(gc src/utils/device-store.ts) -replace '^\s*//(.*fetch.*)', '$1' -replace '^\s*(const hash = document.getElementById.*)', '//$1' | sc src/utils/device-store.ts"
+xcopy ..\via-desktop-cidoo-qk61\* ..\ /E /Y
+rmdir ..\via-desktop-cidoo-qk61 /S /Q
 call npm run build
 xcopy dist\* ..\public\ /E /Y
 
